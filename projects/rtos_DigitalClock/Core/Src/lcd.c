b6928ff2fa093ae99f00d7c2df1a3c52616a8094
Jai-Raja-Saravanam <jairaja250204@gmail.com>
2025-12-05 13:28:55 +0530

    first

---
+++ projects/rtos_DigitalClock/Core/Src/lcd.c
@@ -0,0 +1,95 @@
+#include "lcd.h"
+
+static uint8_t command1[]={0x30,0x30,0x30,0x20,'\0'};
+static uint8_t command[]={0x28,0x0E,0x01,0x06,0x80,'\0'};
+
+
+void lcd_init(void){
+	uint8_t i=0;
+	//while(command[i]!='\0'){
+	//	lcd_write_command(command[i]);
+	//	i++;
+	//}
+	for(i=0;i<4;i++){
+		lcd_write_command8bitmode(command1[i]);
+		HAL_Delay(5);
+	}
+	for(i=0;i<6;i++){
+		lcd_write_command4bitmode(command[i]);
+		HAL_Delay(5);
+	}
+}
+
+void lcd_write_command8bitmode(uint8_t command)
+{
+	uint8_t upper_nibble = command&0xF0;
+	uint8_t backlight = 0x08;
+	uint8_t en = 0x04;
+	uint8_t rw = 0x02;
+	uint8_t rs = 0x01;
+	uint8_t packet = (upper_nibble | backlight | en) & ~(rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet, 1, 10);
+	HAL_Delay(1);
+	packet = (upper_nibble | backlight) & ~(en | rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet, 1, 10);
+	HAL_Delay(5);
+}
+
+
+void lcd_write_command4bitmode(uint8_t command)
+{
+	uint8_t upper_nibble = command&0xF0;
+	uint8_t lower_nibble = (command<<4)&0xF0;
+	uint8_t backlight = 0x08;
+	uint8_t en = 0x04;
+	uint8_t rw = 0x02;
+	uint8_t rs = 0x01;
+
+	//uint8_t packet1 = upper_nibble | backlight ;
+	//packet1 = packet1 | en ;
+	//packet1 = packet1 &(~(rw|rs))  ;
+	uint8_t packet1 = (upper_nibble | backlight | en ) & ~(rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+	packet1 = (upper_nibble | backlight ) & ~(en | rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+	uint8_t packet2 = (lower_nibble | backlight | en ) & ~(rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+	packet2 = (lower_nibble | backlight ) & ~(en | rw | rs);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+}
+
+void lcd_write_data4bitmode(uint8_t *data){
+	while(*data != '\0')
+	{
+	uint8_t upper_nibble = *data&0xF0;
+	uint8_t lower_nibble = (*data<<4)&0xF0;
+	uint8_t backlight = 0x08;
+	uint8_t en = 0x04;
+	uint8_t rw = 0x02;
+	uint8_t rs = 0x01;
+	//uint8_t packet1 = upper_nibble | backlight | rs ;
+	//packet1 = packet1 | en ;
+	//packet1 = packet1 &(~rw)  ;
+	uint8_t packet1 = (upper_nibble | backlight | en | rs) & ~(rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+	//packet1 = packet1 &(~en)  ;
+	packet1 = (upper_nibble | backlight | rs) & ~(en | rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet1, 1, 10);
+	HAL_Delay(2);
+
+	uint8_t packet2 = (lower_nibble | backlight | en | rs) & ~(rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+	packet2 = (lower_nibble | backlight | rs) & ~(en | rw);
+	HAL_I2C_Master_Transmit(&hi2c1, DEVICE_ADDR, &packet2, 1, 10);
+	HAL_Delay(2);
+
+	data++;
+	}
+}
+
---
+++ projects/rtos_DigitalClock/Debug/common/ThirdParty/FreeRtos/tasks.d
@@ -0,0 +1,83 @@
+common/ThirdParty/FreeRtos/tasks.o: \
+ D:/emcog/rtos/common/ThirdParty/FreeRtos/tasks.c ../Core/Inc/main.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ ../Core/Inc/stm32f1xx_hal_conf.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ ../Drivers/CMSIS/Include/core_cm3.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rtc.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rtc_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/portable.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/list.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/timers.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/task.h \
+ D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/stack_macros.h
+../Core/Inc/main.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+../Core/Inc/stm32f1xx_hal_conf.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+../Drivers/CMSIS/Include/core_cm3.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rtc.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rtc_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/portable.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/task.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/list.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/timers.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/task.h:
+D:/emcog/rtos/rtos_codes/RTOS-in-stm32f103c8t6/common/ThirdParty/FreeRTOS/include/stack_macros.h:
